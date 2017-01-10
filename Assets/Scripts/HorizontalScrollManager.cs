33b8dc74cf132081d02bac064298015372df7542
Parth <parth@timeloop.in>
2017-01-10 20:47:40 +0530

    Added partial structure for horizontal scroll

---
+++ Assets/Scripts/HorizontalScrollManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace HorizontalScroll
+{
+	public class HorizontalScrollManager : MonoBehaviour
+	{
+		public GameObject ElementPrefab;
+		public GameObject ScrollParent;
+		public List<ScrollElementInfo> ListOfElementInfo;
+	}
+}
---
+++ Assets/Scripts/ScrollElement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace HorizontalScroll
+{
+	
+	public class ScrollElement : MonoBehaviour
+	{
+
+
+	}
+
+	[System.Serializable]
+	public class ScrollElementInfo
+	{
+		public int ElementId = -1;
+		public string ElementName = string.Empty;
+	}
+
+}
